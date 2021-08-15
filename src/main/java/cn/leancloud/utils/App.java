46b099791d6a63c312cd8fc51fb31f01d01bff81
jwfing <jwfing@gmail.com>
2021-08-15 18:53:12 +0800

    init version

---
+++ src/main/java/cn/leancloud/utils/App.java
@@ -0,0 +1,13 @@
+package cn.leancloud.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
